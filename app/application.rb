bb6cbb30a1f8840c1986c6e62a1c620aa314b9c2
Rori Hinds <r_ori@hotmail.co.uk>
2019-06-01 16:44:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
