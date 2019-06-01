543de93a22dcbe8aa2f4ff9f09cfba45ad30aa86
Rori Hinds <r_ori@hotmail.co.uk>
2019-06-01 16:46:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,6 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    
     else
       
       resp.write "Path Not Found"
